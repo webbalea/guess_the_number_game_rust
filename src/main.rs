4445ff3cbc5ba9af7bdf08054bc0e6eb6c56e794
webbalea <webbalea@gmail.com>
2024-02-25 07:28:18 +0200

    improvements

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ use std::cmp::Ordering;
 use colored::Colorize;
 use rand::Rng;
 
-
 fn main() {
-    println!("Guess the number");
-    println!("Between 1 and 100");
+    println!("Guess the number!\nBetween 1 and 100.");
 
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     loop {
-        println!("Please enter your guess");
+        // Better user prompt.
+        print!("Enter your guess: ");
+        io::stdout().flush().unwrap();
 
         let mut guess = String::new();
 
@@ -21,14 +21,17 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue
+            Err(_) => {
+                println!("{}", "Invalid number! Please enter a number.".yellow());
+                continue
+            }
         };
-        
+
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("{}", "Too small".red()),
-            Ordering::Greater => println!("{}", "Too small".red()),
+            Ordering::Less => println!("{}", "Too small!".bright_red()),  //Improve the visibility of text
+            Ordering::Greater => println!("{}", "Too big!".bright_red()), //Correct text to "Too big!"
             Ordering::Equal => {
-                println!("{}", "You guessed it right!".green());
+                println!("{}", "You guessed it right!".bright_green());
                 break;
             }
         }
